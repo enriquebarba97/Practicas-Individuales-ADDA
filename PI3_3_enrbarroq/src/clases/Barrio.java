bdfca29848e2721f7efbafe6c926c02700d73e86
Enrique Barba <enriquebarba97@gmail.com>
2019-03-11 18:21:36 +0100

    Práctica Individual 3 añadida
    Ejercicio 1 casi completo

---
+++ PI3_3_enrbarroq/src/clases/Barrio.java
@@ -0,0 +1,52 @@
+package clases;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Barrio {
+	
+	public static Barrio create(String s) {
+		return new Barrio(s);
+	}
+	
+	private static Integer nid = 0;
+	
+	private Integer id;
+	private List<Integer> vecinos;
+	
+	Barrio(String s) {
+		String[] vals = s.split("[,]");
+		this.id = nid;
+		nid++;
+		List<Integer> vec = new ArrayList<>();
+		for(int i = 0; i<vals.length; i++) {
+			vec.add(Integer.parseInt(vals[i]));
+		}
+		this.vecinos = vec;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public List<Integer> getVecinos() {
+		return vecinos;
+	}
+
+	public void setVecinos(List<Integer> vecinos) {
+		this.vecinos = vecinos;
+	}
+
+	
+	public String toString() {
+		return "Barrio [id=" + id + ", vecinos=" + vecinos + "]";
+	}
+	
+	
+	
+	
+}
---
+++ PI3_3_enrbarroq/src/ejercicios/Ejercicio1.java
@@ -0,0 +1,56 @@
+package ejercicios;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import clases.Barrio;
+import us.lsi.common.Streams2;
+import us.lsi.lpsolve.solution.AlgoritmoPLI;
+import us.lsi.lpsolve.solution.SolutionPLI;
+
+public class Ejercicio1 {
+
+	public static void main(String[] args) {
+		System.out.println(getConstraints());
+//		SolutionPLI a = AlgoritmoPLI.getSolutionFromFile("ficheros/ejemplo1.txt");
+//		System.out.println("-------------------");	
+//		System.out.println("________");
+//		System.out.println(a.getGoal());
+//		for (int j = 0; j < a.getNumVar(); j++) {
+//			System.out.println(a.getName(j)+" = "+a.getSolution()[j]);
+//		}
+//		System.out.println("________");
+//		System.out.println(a.getGoal());
+	}
+	
+	private static String getConstraints(){
+		List<Barrio> barrios = Streams2.fromFile("ficheros/ejercicio1.txt")
+				.map(s -> Barrio.create(s))
+				.collect(Collectors.toList());
+		String result = "";
+		result += "min:";
+		for(int i = 0; i<barrios.size(); i++) {
+			Barrio b = barrios.get(i);
+			if(i!=0) result += "+";
+			result += "b" + b.getId();
+		}
+		result += ";\n\n";
+		for(int i = 0; i<barrios.size(); i++) {
+			List<Integer> b = barrios.get(i).getVecinos();
+			for(int j = 0; j<b.size();j++) {
+				if(j!=0) result += "+";
+				result += "b" + b.get(j);
+			}
+			result += ">=1;\n";
+		}
+		result += "\n";
+		
+		result += "bin ";
+		for(int i = 0; i<barrios.size(); i++) {
+			if(i != 0) result += ",";
+			result += "b" + barrios.get(i).getId();
+		}
+		result += ";";
+		return result;
+	}
+}
