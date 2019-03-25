891e2e8190fda7abf175bbde1462d2eb0c7816e3
Enrique Barba <enriquebarba97@gmail.com>
2019-03-25 18:27:04 +0100

    Ejercicio 4 apartado b

--- PI3_3_enrbarroq/src/ejercicio4/Ejercicio4.java
+++ PI3_3_enrbarroq/src/ejercicio4/Ejercicio4.java
@@ -3,6 +3,7 @@ package ejercicio4;
 import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.jgrapht.Graph;
 import org.jgrapht.alg.connectivity.ConnectivityInspector;
@@ -23,7 +24,7 @@ public class Ejercicio4 {
 		Graph<Monumento, Camino> grafo1 = GraphsReader.newGraph("ficheros/grafo1.txt", 
 				Monumento::create, 
 				Camino::create, 
-				() -> new SimpleDirectedWeightedGraph<>(Monumento::create,Camino::create), 
+				() -> new SimpleWeightedGraph<>(Monumento::create,Camino::create), 
 				Camino::getTiempo);
 		
 		Graph<Monumento, Camino> grafo2 = GraphsReader.newGraph("ficheros/grafo2.txt", 
@@ -36,20 +37,21 @@ public class Ejercicio4 {
 		System.out.println("¿Es conexo?: " + grafoCon.isConnected());
 		System.out.println("Componentes conexas: " + grafoCon.connectedSets());
 		
-		TopologicalOrderIterator<Monumento, Camino> grafoTopo = new TopologicalOrderIterator<>(grafo1);
+		List<Monumento> visita = grafo2.vertexSet().stream()
+				.filter(v -> grafo2.inDegreeOf(v)==0)
+				.collect(Collectors.toList());	
+		System.out.println("Los monumentos que puedes visitar sin visitar otro antes son: "+visita);
 		
-		List<Monumento> ordenTopo = new ArrayList<>();
-		grafoTopo.forEachRemaining(v->ordenTopo.add(v));
-		System.out.println(ordenTopo);
-			
+		DOTExporter<Monumento,Camino> de1 = new DOTExporter<Monumento,Camino>(
+				new IntegerComponentNameProvider<>(),
+				x->x.getNombre(), 
+				x-> "T = " +x.getTiempo());
 		
-//		DOTExporter<Monumento,Camino> de1 = new DOTExporter<Monumento,Camino>(
-//				new IntegerComponentNameProvider<>(),
-//				x->x.getNombre(), 
-//				x->x.getId()+"--"+x.getTiempo());
-//		
-//		PrintWriter f = Files2.getWriter("ficheros/grafo1.gv");
-//		de1.exportGraph(grafo, f);
+		PrintWriter f = Files2.getWriter("ficheros/grafo1.gv");
+		de1.exportGraph(grafo1, f);
+		
+		PrintWriter f2 = Files2.getWriter("ficheros/grafo2.gv");
+		de1.exportGraph(grafo2, f2);
 	}
 
 }
